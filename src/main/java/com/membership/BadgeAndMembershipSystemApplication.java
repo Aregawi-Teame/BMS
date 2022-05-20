957f184aa05ecbe7b442cf7f7504a9f51df4bfc9
Aregawi-Teame <aregawiyeheys@gmail.com>
2022-05-19 23:37:16 -0500

    Final Final

--- src/main/java/com/membership/BadgeAndMembershipSystemApplication.java
+++ src/main/java/com/membership/BadgeAndMembershipSystemApplication.java
@@ -2,10 +2,8 @@ package com.membership;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class BadgeAndMembershipSystemApplication {
 
 	public static void main(String[] args) 
